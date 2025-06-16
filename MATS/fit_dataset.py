453870b8a31d65e8924f8545f26903cb4a809971
Erin Adkins <ema3@nist.gov>
2025-06-16 09:19:16 -0400

    Changed the line mixing parameterization to be based on the depleted intensity rather than the zero P intensity.

--- MATS/fit_dataset.py
+++ MATS/fit_dataset.py
@@ -225,9 +225,15 @@ def HTP_from_DF_select(linelist, waves, wing_cutoff = 25, wing_wavenumbers = 25,
         lineshape_vals_real, lineshape_vals_imag = pcqsdhc(line['nu'],line['GammaD'],line['Gamma0'],line['Gamma2'],line['Shift0'],line['Shift2'],
                                                     line['NuVC'],line['Eta'],wavenumbers[BoundIndexLower:BoundIndexUpper])
         if BIA_slope:
+            '''
             Xsect[BoundIndexLower:BoundIndexUpper] += mol_dens  * \
                                                         molefraction[line['molec_id']] * line['abun_ratio'] * \
-                                                        (line['LineIntensity_BIA'] * lineshape_vals_real + line['LineIntensity']*line['Y']*lineshape_vals_imag)                               
+                                                        (line['LineIntensity_BIA'] * lineshape_vals_real + line['LineIntensity']*line['Y']*lineshape_vals_imag)  
+            '''
+            Xsect[BoundIndexLower:BoundIndexUpper] += mol_dens  * \
+                                                        molefraction[line['molec_id']] * line['abun_ratio'] * \
+                                                        line['LineIntensity_BIA'] * ( lineshape_vals_real + line['Y']*lineshape_vals_imag)     
+                                     
         else:
             Xsect[BoundIndexLower:BoundIndexUpper] += mol_dens  * \
                                                     molefraction[line['molec_id']] * line['abun_ratio'] * \
