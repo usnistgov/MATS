520c6aae9422473465e82971983f3b60df4a4d19
Erin Adkins <ema3@nist.gov>
2026-02-20 12:37:28 -0500

    removed reindexing

--- MATS/generate_fitparam_file.py
+++ MATS/generate_fitparam_file.py
@@ -189,7 +189,7 @@ class Generate_FitParam_File:
         param_linelist_df = param_linelist_df[param_linelist_df['nu'] > (dataset_min - self.sim_window)]
 
         param_linelist_df.sort_values('nu', inplace = True)
-        param_linelist_df.reset_index(drop = True, inplace = True)
+        #param_linelist_df.reset_index(drop = True, inplace = True)
         
         #delete parameters not partaining to species (where relevant)
         diluent_list = []
